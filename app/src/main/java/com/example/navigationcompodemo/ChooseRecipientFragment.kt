7191fb62cbb2a0f6f1ef8e6a60d088994a78652d
ankitbajaj1989 <ankitbajaj1989@gmail.com>
2020-06-20 00:30:35 +0530

    Added Animation

--- app/src/main/java/com/example/navigationcompodemo/ChooseRecipientFragment.kt
+++ app/src/main/java/com/example/navigationcompodemo/ChooseRecipientFragment.kt
@@ -6,11 +6,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_choose_recipient.*
 
 /**
  * A simple [Fragment] subclass.
  */
 class ChooseRecipientFragment : Fragment() {
+
+    lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,5 +25,18 @@ class ChooseRecipientFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_choose_recipient, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        navController = Navigation.findNavController(view)
+
+        next_btn.setOnClickListener {
+            navController.navigate(R.id.action_chooseRecipientFragment_to_specifyAmountFragment)
+        }
+
+        cancel_btn.setOnClickListener {
+            activity!!.onBackPressed()
+        }
+
+    }
 }
--- app/src/main/java/com/example/navigationcompodemo/SpecifyAmountFragment.kt
+++ app/src/main/java/com/example/navigationcompodemo/SpecifyAmountFragment.kt
@@ -6,11 +6,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_specify_amount.*
 
 /**
  * A simple [Fragment] subclass.
  */
 class SpecifyAmountFragment : Fragment() {
+
+    lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,5 +25,18 @@ class SpecifyAmountFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_specify_amount, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navController = Navigation.findNavController(view)
+
+        send_btn.setOnClickListener {
+            navController.navigate(R.id.action_specifyAmountFragment_to_confirmationFragment)
+        }
+
+        cancel_btn.setOnClickListener {
+            activity!!.onBackPressed()
+        }
+    }
 
 }
