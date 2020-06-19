e8e3891a694eaafbe28f8709e35ab6c0a056eed4
ankitbajaj1989 <ankitbajaj1989@gmail.com>
2020-06-19 23:50:24 +0530

    Added fragments and main activity with NavigationHostFragment setup

--- app/src/main/java/com/example/navigationcompodemo/MainFragment.kt
+++ app/src/main/java/com/example/navigationcompodemo/MainFragment.kt
@@ -6,11 +6,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_main.*
 
 /**
  * A simple [Fragment] subclass.
  */
 class MainFragment : Fragment() {
+
+    lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,6 +23,26 @@ class MainFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // This is the nav Controller which has ref to Nav Graph
+        navController = Navigation.findNavController(view)
+
+        view_transactions_btn.setOnClickListener {
+
+        }
+
+        send_money_btn.setOnClickListener {
+
+        }
+
+
+        view_balance_btn.setOnClickListener {
+
+        }
     }
 
 
