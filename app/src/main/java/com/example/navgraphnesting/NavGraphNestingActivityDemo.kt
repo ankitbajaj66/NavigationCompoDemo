4136d2f72941422e0fef920bbc81c5af0298e7e8
ankitbajaj1989 <ankitbajaj1989@gmail.com>
2020-06-21 00:44:10 +0530

    Setup few fragment for nested nav graphs

---
+++ app/src/main/java/com/example/navgraphnesting/NavGraphNestingActivityDemo.kt
@@ -0,0 +1,13 @@
+package com.example.navgraphnesting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.navigationcompodemo.R
+
+class NavGraphNestingActivityDemo : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nav_graph_nesting_demo)
+    }
+}
