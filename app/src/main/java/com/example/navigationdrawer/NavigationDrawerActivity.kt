e3d5a2a6250f8cc3e0ce7fbd6e3a68cfd7c622c3
ankitbajaj1989 <ankitbajaj1989@gmail.com>
2020-06-21 19:41:56 +0530

    1. mapped nav graph items id with menu.xml id 2. Added new activity and added navhostfragment

---
+++ app/src/main/java/com/example/navigationdrawer/NavigationDrawerActivity.kt
@@ -0,0 +1,13 @@
+package com.example.navigationdrawer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.navigationcompodemo.R
+
+class NavigationDrawerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_navigation_drawer)
+    }
+}
