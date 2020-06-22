dc251b2b2bc9185b9189793af4bbd6bfc10fd2db
ankitbajaj1989 <ankitbajaj1989@gmail.com>
2020-06-22 23:27:17 +0530

    Added Few Scenarios for fragment navigation test.

--- app/src/androidTest/java/com/example/navigationcompodemo/AccountNavigationTest.kt
+++ app/src/androidTest/java/com/example/navigationcompodemo/AccountNavigationTest.kt
@@ -2,12 +2,12 @@ package com.example.navigationcompodemo
 
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Espresso.pressBack
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
-import kotlinx.android.synthetic.main.fragment_main.*
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -20,11 +20,22 @@ class AccountNavigationTest {
     @Test
     fun test_ViewTransactionFragmentNavigation() {
 
+// After launching the activity first fragment which is mainfragment will be loaded
         val activityScenario = ActivityScenario.launch(MainActivity::class.java)
 
-        // Nav to View TransactionFragment
+        // perform click to move to Transaction Fragment
         onView(withId(R.id.view_transactions_btn)).perform(click())
 
+        // check for transaction fragment visibility
         onView(withId(R.id.view_transaction_container)).check(matches(isDisplayed()))
+
+        // pressing back button will take user back to main fragment
+        pressBack()
+
+        // Check for main fragment content
+        onView(withId(R.id.main_fragmentcontainer)).check(matches(isDisplayed()))
+
     }
+
+
 }
