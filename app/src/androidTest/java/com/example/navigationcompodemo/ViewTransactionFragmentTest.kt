1af9073c2e5bc1043f6d8e02bc3dab18a227c8f8
ankit.bajaj <ankit.bajaj@globant.com>
2020-06-23 00:59:24 +0530

    Tested Fragment in isolation

---
+++ app/src/androidTest/java/com/example/navigationcompodemo/ViewTransactionFragmentTest.kt
@@ -0,0 +1,25 @@
+package com.example.navigationcompodemo
+
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import org.junit.Assert.*
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+// Tested Fragment in isolation
+@RunWith(AndroidJUnit4ClassRunner::class)
+class ViewTransactionFragmentTest {
+
+    @Test
+    fun testViewTransactionFragment() {
+        val scenario = launchFragmentInContainer<ViewTransactionFragment>()
+
+        // verify current view
+        Espresso.onView(ViewMatchers.withId(R.id.view_transaction_container))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+}
